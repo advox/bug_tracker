59f80118d885297c24b92287fb6e4667a603f35f
michal <xmihau@gmail.com>
2017-03-03 10:56:16 +0100

    master

--- models/task.js
+++ models/task.js
@@ -1,3 +1,20 @@
-/**
- * Created by michal on 3/3/17.
- */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    status: Number,
+    title: String,
+    description: String,
+    rank: Number,
+    important: Number,
+    author: Number,
+    assignee: Number,
+    notifications: Array,
+    files: Array,
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
