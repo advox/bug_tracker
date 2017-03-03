b08409ff0b3f5d469570296b17e196c0876dc9f4
michal <xmihau@gmail.com>
2017-03-03 10:47:00 +0100

    master

---
+++ models/task.js
@@ -0,0 +1,3 @@
+/**
+ * Created by michal on 3/3/17.
+ */
---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    login: String,
+    password: String,
+    email: String,
+    status: Boolean,
+    group: Number,
+    color: String,
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now},
+    lastLogin: {type: Date}
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
