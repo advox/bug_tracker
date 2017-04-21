c061422d778d3bd13ade90d2325b83bab57dd60e
michal nowicki <m.nowicki@advoxstudio.com>
2017-04-21 15:35:24 +0200

    troche widok formularza

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,11 +1,15 @@
 (function($) {
     'use strict';
 
-    $(document).on('click', '#taskDelete button', function(){
+    $(document).on('click', '#task-delete button', function(){
 
         if (confirm("Are you sure?") == false) {
             return false;
         }
+    });
+
+    $('.wysiwyg').summernote({
+        minHeight: 300
     });
 
 })(jQuery);
